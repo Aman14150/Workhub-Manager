1d19d1f0314b31297b20917cfe260f06663327b1
harshmahalle <mahalleharsh9@gmail.com>
2024-12-22 14:47:37 +0530

    route updated

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,18 @@ import taskRoutes from "./taskRoutes.js";
 
 const router = express.Router();
 
+const app = express();
+
 router.use("/user", userRoutes); //api/user/login  // Routes for user-related actions
 router.use("/task", taskRoutes); // Routes for task-related actions
+
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Welcome to the Workhub!',
+      routes: {
+        summary: '/api/summary',
+      },
+    });
+  });
 
 export default router;
